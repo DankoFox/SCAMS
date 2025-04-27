78d69ac65d40fb31af387e0d29f9b83f022c8ed2
DankoFox <timzek7@gmail.com>
2025-04-27 11:42:26 +0700

    [Fix]: wrong path

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 // src/components/NavBar.tsx
 import { useEffect, useState } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../../supabaseClient";
 import { useNavigate } from "react-router-dom";
 
 export default function NavBar() {
