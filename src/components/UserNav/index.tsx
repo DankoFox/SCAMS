63b12c0b9a4aefbff00d3427d2e16ee05f4c0364
Wan-Mink <minh.phan04@hcmut.edu.vn>
2025-04-27 13:06:24 +0700

    update

--- src/components/UserNav/index.tsx
+++ src/components/UserNav/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import style from "./UserNav.module.css"
 
 const TaskBar: React.FC = () => {
   const logoUrl = "https://sso.hcmut.edu.vn/cas/images/bk_logo.png";
@@ -9,140 +10,48 @@ const TaskBar: React.FC = () => {
     console.log("Exit clicked");
   };
 
-  const navStyle: React.CSSProperties = {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "10px 25px",
-    backgroundColor: "#f8f9fa",
-    borderBottom: "1px solid #dee2e6",
-    fontFamily: "Arial, sans-serif",
-    color: "#343a40",
-  };
-
-  const leftSectionStyle: React.CSSProperties = {
-    display: "flex",
-    alignItems: "center",
-    gap: "12px",
-  };
-
-  const logoStyle: React.CSSProperties = {
-    width: "30px",
-    height: "30px",
-    display: "block",
-  };
-
-  const appNameStyle: React.CSSProperties = {
-    fontWeight: "bold",
-    fontSize: "1.1rem",
-  };
-
-  const rightSectionStyle: React.CSSProperties = {
-    display: "flex",
-    alignItems: "center",
-    gap: "10px",
-  };
-
-  const navItemStyle: React.CSSProperties = {
-    display: "flex",
-    alignItems: "center",
-    gap: "6px",
-    textDecoration: "none",
-    color: "inherit",
-    cursor: "pointer",
-    padding: "5px",
-    borderRadius: "4px",
-    transition: "background-color 0.2s ease",
-  };
-
-  const navButtonStyle: React.CSSProperties = {
-    ...navItemStyle, // Inherit base styles
-    background: "none",
-    border: "none",
-    fontFamily: "inherit",
-    fontSize: "inherit",
-  };
-
-  const separatorStyle: React.CSSProperties = {
-    color: "#adb5bd",
-    margin: "0 5px",
-    cursor: "default",
-  };
-
-  // Hover effect )
-  const handleMouseEnter = (e: React.MouseEvent<HTMLElement>) => {
-    e.currentTarget.style.backgroundColor = "#e9ecef";
-  };
-
-  const handleMouseLeave = (e: React.MouseEvent<HTMLElement>) => {
-    e.currentTarget.style.backgroundColor = "transparent";
-  };
+  
 
   return (
-    <nav style={navStyle}>
+    <nav className={style.nav}>
       {}
-      <div style={leftSectionStyle}>
-        <img src={logoUrl} alt="SCAMS Logo" style={logoStyle} />
-        <span style={appNameStyle}>SCAMS</span>
+      <div className={style.leftSectionStyle}>
+        <img src={logoUrl} alt="SCAMS Logo" className={style.logoStyle} />
+        <span className={style.appNameStyle}>SCAMS</span>
       </div>
 
       {}
-      <div style={rightSectionStyle}>
+      <div className={style.rightSectionStyle}>
         {}
-        <a
-          href="#home"
-          style={navItemStyle}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
+        <a href="#home" className={style.navItemStyle}>
           <span>Home</span>
         </a>
 
-        <span style={separatorStyle}>|</span>
+        <span className={style.separatorStyle}>|</span>
 
         {}
-        <a
-          href="#room-system"
-          style={navItemStyle}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
+        <a href="#room-system" className={style.navItemStyle}>
           <span>Room system</span>
         </a>
 
-        <span style={separatorStyle}>|</span>
+        <span className={style.separatorStyle}>|</span>
 
         {}
-        <a
-          href="#my-account"
-          style={navItemStyle}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
+        <a href="#my-account" className={style.navItemStyle}>
           <span>My account</span>
         </a>
 
-        <span style={separatorStyle}>|</span>
+        <span className={style.separatorStyle}>|</span>
 
         {}
-        <a
-          href="#contact"
-          style={navItemStyle}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
+        <a href="#contact" className={style.navItemStyle}>
           <span>Contact</span>
         </a>
 
-        <span style={separatorStyle}>|</span>
+        <span className={style.separatorStyle}>|</span>
 
         {}
-        <button
-          onClick={handleExit}
-          style={{ ...navButtonStyle, marginRight: "10px" }}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
+        <button onClick={handleExit} className={style.navButton}>
           {}
           <FontAwesomeIcon icon={faSignOutAlt} />
           <span>Exit</span>
