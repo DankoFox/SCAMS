0fd10b780e68ee6e3aefd9156a8305ebcd609014
Phan Binh Hao <binhhao612@gmail.com>
2025-05-01 16:19:58 +0700

    thêm route đến landing page cho App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import Login from "./pages/Login";
 import CompleteProfile from "./pages/CompleteProfile";
 import UpdateProfile from "./pages/UpdateProfile";
 import RedirectLogin from "./pages/RedirectLogin";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/landing-page" element={<LandingPage />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/redirect-login" element={<RedirectLogin />} />
 				<Route path="/complete-profile" element={<CompleteProfile />} />
