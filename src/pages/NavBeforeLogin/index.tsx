de17eb8b7c952616a1d2676439ab88a3c3f4970f
Wan-Mink <minh.phan04@hcmut.edu.vn>
2025-05-04 11:52:27 +0700

    fix Img not load

--- src/pages/NavBeforeLogin/index.tsx
+++ src/pages/NavBeforeLogin/index.tsx
@@ -1,36 +1,31 @@
 // e:/Download/SCAMS/src/pages/NavBeforeLogin/index.tsx
-import React from 'react';
-import Img from '../../assets/HCMUT.svg';
-import styles from './NavBeforeLogin.module.css'; // Import the CSS module
+import React from "react";
+import Img from "../../../public/HCMUT.svg";
+import styles from "./NavBeforeLogin.module.css"; // Import the CSS module
 
 const NavBeforeLogin: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.contentWrapper}>
-        <div className={styles.logoContainer}>
-          <img
-            src={Img}
-            alt="Logo"
-            className={styles.logo}
-          />
+    <div>
+      <div className={styles.container}>
+        <div className={styles.contentWrapper}>
+          <div className={styles.logoContainer}>
+            <img src={Img} alt="Logo" className={styles.logo} />
+          </div>
+          <hr className={`${styles.hr} ${styles.hrTop}`} />
+          {}
+          <div>
+            <button className={styles.button} type="button">
+              Students of BackKhoa
+            </button>
+            <button
+              className={`${styles.button} ${styles.secondButton}`}
+              type="button"
+            >
+              Lecturers of Backkhoa
+            </button>
+          </div>
+          <hr className={`${styles.hr} ${styles.hrBottom}`} />
         </div>
-        <hr className={`${styles.hr} ${styles.hrTop}`} />
-        {}
-        <div>
-          <button
-            className={styles.button} 
-            type="button"
-          >
-            Students of BackKhoa
-          </button>
-          <button
-            className={`${styles.button} ${styles.secondButton}`} 
-            type="button"
-          >
-            Lecturers of Backkhoa
-          </button>
-        </div>
-        <hr className={`${styles.hr} ${styles.hrBottom}`} />
       </div>
     </div>
   );
